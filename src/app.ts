186417810455344ad8323530f6919f9ce187aa0a
Nathan Le Granvallet <nathan@legranvallet.fr>
2026-07-05 18:28:19 +0200

    Confirmation email

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,22 @@
 import express from "express";
 import postmark from "postmark";
 
-import { apiKeyAuth } from "./helpers.ts";
-import * as MAIL_DATA from "./mail.json" with { type: "json" };
+import { apiKeyAuth, replacePlaceholders } from "./helpers.ts";
+import fs from "node:fs";
+import path from "node:path";
 
 const SMTP_CLIENT = process.env.SMTP_CLIENT || "";
 const CLIENT = new postmark.ServerClient(SMTP_CLIENT);
+const EA_URL = "https://meetings.crf.tools/index.php/api/v1/";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const HTML = fs.readFileSync(path.resolve(__dirname, "mail.html"), "utf8");
+const TEXT = fs.readFileSync(path.resolve(__dirname, "mail.txt"), "utf8");
 
 const app = express();
 app.disable("x-powered-by");
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/event", (req: express.Request, res: express.Response) => {
   console.log("GET /event", req.query);
@@ -18,27 +25,62 @@ app.get("/event", (req: express.Request, res: express.Response) => {
 
 app.post("/event", async (req: express.Request, res: express.Response) => {
   if (!apiKeyAuth(req)) {
-    console.log("failed");
     return res.status(401).json({ error: "Unauthorized" });
   }
 
-  const body = req.body;
-
-  if (body === undefined) {
+  if (req.body === undefined) {
     return res.status(400).json({ error: "Missing required fields" });
   }
 
-  console.log(body);
+  const rawResponse = await fetch(
+    EA_URL + "customers/" + req.body.payload.id_users_customer,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + process.env.EA_API_KEY,
+      },
+    },
+  );
 
-  const to = body.to || "unknown";
+  if ((await rawResponse.status) !== 200) {
+    console.log(EA_URL + "customers/" + req.body.payload.id_users_customer);
+    console.log("Error fetching customer data:", await rawResponse.text());
+    return res.status(502).json({ error: "Unable to fetch customer data" });
+  }
+
+  const response = (await rawResponse.json()) as any;
+
+  const date = new Date(req.body?.payload?.start_datetime).toLocaleDateString(
+    "fr-FR",
+  );
+  const heure =
+    new Date(req.body?.payload?.start_datetime)
+      .getHours()
+      .toString()
+      .padStart(2, "0") +
+    ":" +
+    new Date(req.body?.payload?.start_datetime)
+      .getMinutes()
+      .toString()
+      .padStart(2, "0");
+  const id = req.body?.payload?.hash;
 
   try {
     await CLIENT.sendEmail({
       From: "benevolat.paris15@croix-rouge.fr",
-      To: to,
-      Subject: MAIL_DATA.default.Subject,
-      HtmlBody: MAIL_DATA.default.HtmlBody,
-      TextBody: MAIL_DATA.default.TextBody,
+      To: response?.email,
+      Subject: "Bienvenue à la Croix-Rouge française de Paris 15 !",
+      HtmlBody: replacePlaceholders(HTML, {
+        name: response?.firstName,
+        date: date,
+        heure: heure,
+        id: id,
+      }),
+      TextBody: replacePlaceholders(TEXT, {
+        name: response?.firstName,
+        date: date,
+        heure: heure,
+      }),
       MessageStream: "outbound",
     });
   } catch {
--- src/helpers.ts
+++ src/helpers.ts
@@ -6,3 +6,16 @@ export const apiKeyAuth = (req: express.Request) => {
   const provided = req.header("x-api-key") || "";
   return provided === API_KEY;
 };
+
+export const replacePlaceholders = (
+  template: string,
+  replacements: Record<string, string>,
+) => {
+  let result = template;
+
+  for (const [placeholder, value] of Object.entries(replacements)) {
+    const regex = new RegExp(`{{${placeholder}}}`, "g");
+    result = result.replace(regex, value);
+  }
+  return result;
+};
